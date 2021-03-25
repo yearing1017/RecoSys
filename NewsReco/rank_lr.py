063da95f9b6481e6ba576c1208af1fb8c1d246d0
yearing1017@126.com <yearing1017@126.com>
2021-03-25 23:02:45 +0800

    update

--- NewsReco/rank_lr.py
+++ NewsReco/rank_lr.py
@@ -7,10 +7,12 @@ import warnings
 import joblib
 import lightgbm as lgb
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+#from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GroupKFold
-from sklearn.preprocessing import LabelEncoder
+#from sklearn.preprocessing import LabelEncoder
 from tqdm import tqdm
-
+from sklearn.metrics import auc,roc_auc_score
 from utils import Logger, evaluate, gen_sub
 
 warnings.filterwarnings('ignore')
@@ -19,7 +21,7 @@ seed = 2020
 random.seed(seed)
 
 # 命令行参数
-parser = argparse.ArgumentParser(description='lightgbm 排序')
+parser = argparse.ArgumentParser(description='LogisticRegression 排序')
 parser.add_argument('--mode', default='valid')
 parser.add_argument('--logfile', default='test.log')
 
@@ -31,7 +33,7 @@ logfile = args.logfile
 # 初始化日志
 os.makedirs('user_data/log', exist_ok=True)
 log = Logger(f'user_data/log/{logfile}').logger
-log.info(f'lightgbm 排序，mode: {mode}')
+log.info(f'LogisticRegression 排序，mode: {mode}')
 
 
 def train_model(df_feature, df_query):
@@ -51,19 +53,7 @@ def train_model(df_feature, df_query):
             lambda x: x not in [ycol, 'created_at_datetime', 'click_datetime'],
             df_train.columns))
     feature_names.sort()
-
-    model = lgb.LGBMClassifier(num_leaves=64,
-                               max_depth=10,
-                               learning_rate=0.05,
-                               n_estimators=10000, #树的数量,即训练轮次
-                               subsample=0.8,
-                               feature_fraction=0.8,
-                               reg_alpha=0.5, # L1正则化系数
-                               reg_lambda=0.5, #L2正则化系数
-                               random_state=seed,
-                               importance_type='gain',
-                               metric=None)
-
+    lr_model = LogisticRegression()
     oof = []
     # 取出这两列的内容
     prediction = df_test[['user_id', 'article_id']]
@@ -77,55 +67,39 @@ def train_model(df_feature, df_query):
     # 我们有时候可能不希望某一组或某一类型的样本被切分到训练集和测试集中，而是希望这组数据全部都在训练集中或者全部在测试集中，
     # 此处将每个用户的全部放到训练集中 或 验证集中  不被分开
     for fold_id, (trn_idx, val_idx) in enumerate(
-            kfold.split(df_train[feature_names], df_train[ycol],df_train['user_id'])):
+        kfold.split(df_train[feature_names], df_train[ycol],df_train['user_id'])):
+    
         X_train = df_train.iloc[trn_idx][feature_names]
         Y_train = df_train.iloc[trn_idx][ycol]
 
         X_val = df_train.iloc[val_idx][feature_names]
         Y_val = df_train.iloc[val_idx][ycol]
-
+        
+        
         log.debug(
             f'\nFold_{fold_id + 1} Training ================================\n'
         )
 
-        lgb_model = model.fit(X_train,
-                              Y_train,
-                              eval_names=['train', 'valid'],
-                              eval_set=[(X_train, Y_train), (X_val, Y_val)],
-                              verbose=100,
-                              eval_metric='auc',
-                              early_stopping_rounds=100)
+        lr_model.fit(X_train, Y_train)
+
         # 验证集结果的计算
         # 计算类别1的auc，则要用样本预测为1的概率model.predict_proba(testdata)[:,1]
-        pred_val = lgb_model.predict_proba(
-            X_val, num_iteration=lgb_model.best_iteration_)[:, 1]
+        pred_val = lr_model.predict_proba(X_val)[:, 1]
+        auc_val = roc_auc_score(Y_val, pred_val)
+        print('Fold:{}, auc is:{}:'.format(fold_id+1, auc_val)) 
         # 拷贝的验证集的三列
         df_oof = df_train.iloc[val_idx][['user_id', 'article_id', ycol]].copy()
         df_oof['pred'] = pred_val # 填入该样本被预测为1的概率
         oof.append(df_oof) # 总的oof包含k折验证的所有验证集的结果
 
         # 测试集结果的计算
-        pred_test = lgb_model.predict_proba(
-            df_test[feature_names], num_iteration=lgb_model.best_iteration_)[:,1]
+        pred_test = lr_model.predict_proba(df_test[feature_names])[:,1]
         # 将5次测试的结果加和除5 填入prediction
         prediction['pred'] += pred_test / 5
 
-        df_importance = pd.DataFrame({
-            'feature_name':
-            feature_names,
-            'importance':
-            lgb_model.feature_importances_,
-        })
-        df_importance_list.append(df_importance)
         # 保存模型
-        joblib.dump(model, f'user_data/model/lgb{fold_id}.pkl')
+        #joblib.dump(model, f'user_data/model/lgb{fold_id}.pkl')
     # k折训练结束后
-    # 特征重要性
-    df_importance = pd.concat(df_importance_list)
-    df_importance = df_importance.groupby([
-        'feature_name'
-    ])['importance'].agg('mean').sort_values(ascending=False).reset_index()
-    log.debug(f'importance: {df_importance}')
 
     # 生成线下，df_oof为df_train所有的投入到训练的用户的排序结果
     df_oof = pd.concat(oof)
@@ -183,10 +157,11 @@ if __name__ == '__main__':
         df_feature = pd.read_pickle('user_data/data/offline/feature.pkl')
         df_query = pd.read_pickle('user_data/data/offline/query.pkl')
         # 类别型特征
+        '''
         for f in df_feature.select_dtypes('object').columns:
             lbl = LabelEncoder() # 将n个类别编码为0~n-1之间的整数（包含0和n-1）
             df_feature[f] = lbl.fit_transform(df_feature[f].astype(str))
-
+        '''
         train_model(df_feature, df_query)
     else:
         df_feature = pd.read_pickle('user_data/data/online/feature.pkl')
